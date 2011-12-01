d5ad2e632416c8693d59b127a3a45eaee46f02af
felixtrepanier <felixtrepanier@gmail.com>
2011-11-30 22:26:45 -0500

    try with remote server access

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,7 +2,15 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import sun.net.www.http.HttpClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.*;
 
 import models.*;
@@ -10,7 +18,18 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	try {
+			URLConnection c = new URL("http://www.rhok.org").openConnection();
+			InputStream is = c.getInputStream();
+			InputStreamReader reader = new InputStreamReader(is);
+			BufferedReader breader = new BufferedReader(reader);
+			String line = breader.readLine();
+			is.close();
+			render(line);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
     }
 
 }
