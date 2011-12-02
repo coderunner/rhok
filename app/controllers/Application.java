5c512eccacaf8d25bdc9d8af20788c49274f641c
Alexandre Normand <alexandre.normand@nokia.com>
2011-12-02 13:46:11 -0500

    Clean up unused imports.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,11 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-import sun.net.www.http.HttpClient;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.*;
-
-import models.*;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
